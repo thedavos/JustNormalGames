35e15b9f4f45f76309bcdda5592a3aa7fcf0bbc4
davidvs <davidvargas.d45@gmail.com>
2018-11-14 19:39:28 -0500

    translation complete

--- index.js
+++ index.js
@@ -5,32 +5,82 @@ $(() => {
         let index = 0
         const gamesImages = Array.from($('.hero-item').children('img'))
         const gamesItems = Array.from($('.hero-games').children())
-        setInterval(() => {
 
-            if(count === 360) {
+        function carouselEngine() {
+            if (count === 360) {
                 count = 0
             }
 
             if (count === 268) {
-                
+
                 if (index > gamesImages.length - 1) {
                     index = 0
                 }
 
                 $('.header-image').attr('src', `${gamesImages[index].src}`);
-                
+
                 index++
             }
 
             count++
             $('.header-image').css('transform', `rotateX(${count}deg)`)
-        }, 10)
+        }
+
+        const intervalo = setInterval(carouselEngine, 10)
 
         $('#main-btn').click(function (e) {
             window.open(gamesItems[index != 4 ? index : 3].href)
         });
+        
+        let status = true
+        $('.header-image').click(function() {
+            if (status) {
+                clearInterval(intervalo)
+                status = false
+            } else {
+                rotateBanner()
+                status = true
+            }
+        })
     }
 
     rotateBanner()
 
+    function isCorrect(input) {
+        input.css('border', 'none')
+        return swal(
+            'Suscrito!',
+            'Cada semana publicamos un nuevo artículo!',
+            'success'
+        )
+    }
+
+    function isIncorrect(input) {
+        input.css('border', '2px solid #ffb5b5')
+        return swal(
+            'Oops...',
+            'Al parecer hubo un error con tu email',
+            'error'
+        )
+    }
+
+    function validateForm() {
+
+        const regexEmail = /^[\w\.-_!#$%&-]+@[a-z]+\.[a-z]{2,4}$/
+        
+        $('#newsletter-form').submit(function (e) { 
+            e.preventDefault();
+
+            const email = $('#email')
+            const emailValue = $('#email').val()
+
+            if (regexEmail.test(emailValue)) {
+                isCorrect(email)
+            } else {
+                isIncorrect(email)
+            }
+        });
+    }
+
+    validateForm()
 })
--- src/js/translation.js
+++ src/js/translation.js
@@ -1,15 +1,97 @@
 i18next.init({
     lng: 'en',
-    debug: true,
     resources: {
         en: {
             translation: {
-                "key": "hola mundo!"
+                // FOOTER
+                footerLinks: {
+                    contact: 'Contact',
+                    source: 'Source Code',
+                    team: 'Team',
+                    sitemap: 'SiteMap'
+                },
+                copy: {
+                    msg: '© Copyright Cibertec Developers Team 2018'
+                },
+                option: {
+                    en: 'English',
+                    es: 'Spanish'
+                },
+                // TITLES
+                titles: {
+                    news: 'Last News',
+                    games: 'Javascript Games',
+                    play: 'Play Game'
+                },
+                // NAV
+                nav: {
+                    games: 'Games',
+                    subscription: 'Subscription',
+                    news: 'News'
+                },
+                news: {
+                    more: 'See more'
+                }
+            }
+        },
+        es: {
+            translation: {
+                // FOOTER
+                footerLinks: {
+                    contact: 'Contacto',
+                    source: 'Código Fuente',
+                    team: 'Equipo',
+                    sitemap: 'Mapa de Sitio'
+                },
+                copy: {
+                    msg: '© Equipo de Desarrolladores Cibertec 2018'
+                },
+                option: {
+                    en: 'Inglés',
+                    es: 'Español'
+                },
+                // TÍTULOS
+                titles: {
+                    news: 'Últimas Noticias',
+                    games: 'Juegos Javascript',
+                    play: 'Juega'
+                },
+                // NAVEGADOR
+                nav: {
+                    games: 'Juegos',
+                    subscription: 'Suscripción',
+                    news: 'Noticias'
+                },
+
+                news: {
+                    more: 'Ver más'
+                }
             }
         }
     }
 }, function (err, t) {
-    // initialized and ready to go!
-   
-    /* document.getElementById('output').innerHTML = i18next.t('key'); */
+    i18nextJquery.init(i18next, $);
+
+    $('#selectLanguage').change(function () {
+
+        i18next.changeLanguage(this.value)
+        $('.footer-links').localize()
+        $('.footer-copy').localize()
+        $('#selectLanguage').localize()
+        $('#main-btn').localize()
+        $('.navbar-list').localize()
+        $('.news-card-more').localize()
+        $('h2').localize()
+        changeSuscriptionText(this.value)
+    });
 });
+
+function changeSuscriptionText(value) {
+    if (value === 'es-ES') {
+        $('#email').attr('placeholder', 'Deja tu e-mail aquí si quieres conocer las últimas noticias!')
+        $('#btnSuscription').attr('value', 'SUSCRÍBETE')
+    } else {
+        $('#email').attr('placeholder', 'Leave your e-mail here if you want to know the last games and news!')
+        $('#btnSuscription').attr('value', 'SUSCRIBE')
+    }
+}
